f393e3b081dc04077bdb614b153b8abe900c3c2f
MiShoo22 <mishoberdzenisvili2002@gmail.com>
2023-01-31 05:11:51 +0400

    second commit

---
+++ day 2/lesson2.py
@@ -0,0 +1,15 @@
+from operator import  is_
+
+name="misho"
+surname="berdzenishvili"
+age="20"
+height="175"
+identity="10203040"
+is_man="true"
+knows_programing="false"
+nikcname="miSho22"
+love="football"
+hobby="cooking"
+
+
+print(name+"  " + surname+"  "+str(age)+" "+str(height)+" " +identity+" "+str(is_man)+"  "+ str(knows_programing)+" "+nikcname+" "+ love+ ""+hobby+" " )
